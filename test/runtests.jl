d00cac0d17f1e04b439d58e30bed805865e12434
gaelforget <gforget@mit.edu>
2021-02-27 02:39:55 -0500

    improve test coverage 3/n

--- test/runtests.jl
+++ test/runtests.jl
@@ -25,6 +25,40 @@ for nTopo=1:3
 end;
 end;
 
+@testset "Helmholtz decomposition test:" begin
+
+    function trsp_read(myspec::String,mypath::String)
+        γ=GridSpec(myspec,mypath)
+        TrspX=γ.read(mypath*"TrspX.bin",MeshArray(γ,Float32))
+        TrspY=γ.read(mypath*"TrspY.bin",MeshArray(γ,Float32))
+        TauX=γ.read(mypath*"TauX.bin",MeshArray(γ,Float32))
+        TauY=γ.read(mypath*"TauY.bin",MeshArray(γ,Float32))
+        SSH=γ.read(mypath*"SSH.bin",MeshArray(γ,Float32))
+        return TrspX, TrspY, TauX, TauY, SSH
+    end
+    
+Γ=GridLoad(GridSpec("LatLonCap",MeshArrays.GRID_LLC90))
+μ =Γ["hFacC"][:,1]; μ[findall(μ.>0.0)].=1.0; μ[findall(μ.==0.0)].=NaN
+(Tx,Ty,τx,τy,η)=trsp_read("LatLonCap",MeshArrays.GRID_LLC90);
+
+TrspCon=μ.*convergence(Tx,Ty)
+#scalar potential
+TrspPot=ScalarPotential(TrspCon)
+#Divergent transport component
+(TxD,TyD)=gradient(TrspPot,Γ)
+TxD=TxD.*Γ["DXC"]
+TyD=TyD.*Γ["DYC"]
+#Rotational transport component
+TxR = Tx-TxD
+TyR = Ty-TyD
+#vector Potential
+TrspPsi=VectorPotential(TxR,TyR,Γ);
+
+tst=extrema(write(TrspPsi))
+
+@test prod(isapprox.(tst,(-8.08f7,2.19f8)))
+end;
+
 @testset "doctests" begin
     doctest(MeshArrays; manual = false)
 end
