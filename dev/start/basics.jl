6b10bf33d4f810dcae6783b4c0f26815a95974a2
Documenter.jl <documenter@juliadocs.github.io>
2021-10-13 05:46:56 +0000

    build based on fab9ba1

--- dev/start/basics.jl
+++ dev/start/basics.jl
@@ -4,13 +4,22 @@
 using Markdown
 using InteractiveUtils
 
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : missing
+        el
+    end
+end
+
 # ╔═╡ 1a714fba-2a8e-11ec-182f-8f85cc17b02a
 begin
 	using Pkg
 	Pkg.activate("../docs")
 	
 	using MeshArrays
-	
+		
 	import PlutoUI
 	import CairoMakie as Mkie
 	toc=PlutoUI.TableOfContents()
@@ -22,7 +31,7 @@ begin
 	"""
 end
 
-# ╔═╡ 0dbf6e21-2dd7-497f-9b0b-f67707f2e7e0
+# ╔═╡ 7c514f3b-f04d-49e3-9640-d17fdef392e6
 module myDemos
 	using MeshArrays
 	p=dirname(pathof(MeshArrays))
@@ -33,7 +42,7 @@ end
 md"""# Get Started
 
 !!! tip
-    You can download this notebook [here](https://raw.githubusercontent.com/JuliaClimate/MeshArrays.jl/master/examples/basics.jl) and experience it interactively using [Pluto.jl](https://plutojl.org). This can be a good way to get familiar with software such as this one.
+    You can download this notebook or simply read its code in detail [here](https://raw.githubusercontent.com/JuliaClimate/MeshArrays.jl/master/examples/basics.jl). Running notebooks interactively using [Pluto.jl](https://plutojl.org) can be a good way to get familiar with software such as this one.
 
 To create your first `MeshArray`, open the `Julia` REPL and type:
 
@@ -54,44 +63,48 @@ end
 # ╔═╡ a0a7637f-f115-4257-a140-06dc48dd5fba
 md"""## Tutorial
 
-In this tutorial, the same example is repeated three times -- for three different grid configurations commonly used in numerical models (see [Grids](https://juliaclimate.github.io/MeshArrays.jl/dev/start/#Grids)). The example proceeds as follows in each grid case:
+The chosen example proceeds as follows:
 
-1. generate grid configuration
-1. initialize map with random noise
-1. apply smoothing across whole domain
-1. plot results for the subdomain arrays
+1. generate global grid configuration
+1. initialize random noise field
+1. apply smoothing
+1. plot results
 
-Step 3 illustrates how `MeshArrays.jl` computes partial derivatives between neighboring subdomains. The `MeshArrays.smooth()` method is indeed based on lateral diffusion, a transport process, which is integrated through time over the global domain. Other processes like e.g. advection by ocean currents would work similarly.
+Step 3 illustrates how `MeshArrays.jl` computes partial derivatives between neighboring subdomains of the global domain. The chosen smoothing method (`MeshArrays.smooth`) indeed integrates lateral diffusion, a transport process, over time and the global domain. Other processes like e.g. advection by ocean currents would work similarly.
+
+Below, this example is repeated for three different grid configurations commonly used in numerical models (see [Grids](https://juliaclimate.github.io/MeshArrays.jl/dev/start/#Grids)). 
 """
 
 # ╔═╡ 54030bff-45f7-43b1-b19f-7c0cd05a02ae
-md"""### 1. Doubly Periodic Domain
-
-Let's start with a doubly periodic domain split into `nF=16` subdomains. Each subdomain corresponds to an array of `nP=20` by `nQ=20` grid points. 
-The `UnitGrid()` function is used to generate such a grid configuration.
-"""
+begin
+	nP_s = @bind nP PlutoUI.NumberField(10:20:30, default=20)
+	nQ_s = @bind nQ PlutoUI.NumberField(10:20:30, default=30)
+	mP_s = @bind mP PlutoUI.NumberField(1:4, default=4)
+	mQ_s = @bind mQ PlutoUI.NumberField(1:4, default=2)
+	md"""### 1. Doubly Periodic Domain
+	
+	Let's start with a doubly periodic domain split into `mP*mQ` subdomains. 
+	Each subdomain corresponds to an array of `nP,nQ` grid points. 
+	The `UnitGrid()` function is used to generate such a grid configuration.
+	
+	nP x mP = $(nP_s) x $(mP_s)
+		
+	nQ x mQ = $(nQ_s) x $(mQ_s)
+	
+	`(Γ,γ)=UnitGrid( (nP*mP,nQ*mQ) , (nP,nQ) ; option="full")`
+	"""
+end
 
 # ╔═╡ 1fc99fca-3440-4765-b11d-e10eb560aff7
 begin
-	(nP,nQ,nF)=(20,20,16)
-	facesSize=fill((nP,nQ),nF)
-	ioSize=[nP nQ*nF]
-	#ioSize=[nP*4 nQ*4]
+	(Γ,γ)=UnitGrid( (nP*mP,nQ*mQ) , (nP,nQ) ; option="full")
 	
-	γ=gcmgrid("","PeriodicDomain",nF,facesSize, ioSize,Float32, read, write)
-	Γ=UnitGrid(γ;option="full")
-	
-	"Done with basic grid"
+	PlutoUI.with_terminal() do
+		show(Γ.XC)
+	end
 end
 
 # ╔═╡ cef83984-80dc-4196-8edd-314885fa9448
-md"""Then we do steps 2 (`zin`) and 3 (`zout`) as follows. 
-
-!!! note
-    The `heatmap` display should reflect that `zout` is smoother (and therefore muted) than is `zin`.
-"""
-
-# ╔═╡ 2573fda7-66cb-484f-9d06-a9bb85a26a9e
 begin
 	#initialize 2D field of random numbers
 	tmp1=randn(Float32,Tuple(γ.ioSize))
@@ -102,17 +115,28 @@ begin
 	
 	#apply smoother
 	zout=smooth(zin,Lx,Ly,Γ)
-	
+
+	md"""Then we do steps 2 (`zin`) and 3 (`zout`) in this code cell, and plot results in the next. 
+
+	!!! note
+	    The `heatmap` display should reflect that `zout` (r.h.s.) is smoother, and therefore more muted, than is `zin` (l.h.s).
+	"""
+end
+
+# ╔═╡ 2573fda7-66cb-484f-9d06-a9bb85a26a9e
+begin	
 	#visualize
-	x=write(Γ.XC)[:,1]
-	y=write(Γ.YC)[1,:]
+	x=γ.write(Γ.XC)[:,1]
+	y=γ.write(Γ.YC)[1,:]
+	#x=0.5:ioSize[1]-0.5
+	#y=0.5:ioSize[2]-0.5
 	
 	fig = Mkie.Figure(resolution = (900,900), backgroundcolor = :grey95)
 
 	ax1 = Mkie.Axis(fig[1,1])
-	hm1=Mkie.heatmap!(ax1,x,y,write(zin),clims=(-0.25,0.25),tickfont = (4, :black))
+	hm1=Mkie.heatmap!(ax1,x,y,γ.write(zin),clims=(-0.25,0.25),tickfont = (4, :black))
 	ax2 = Mkie.Axis(fig[1,2])
-	hm2=Mkie.heatmap!(ax2,x,y,write(zout),clims=(-0.25,0.25),tickfont = (4, :black))
+	hm2=Mkie.heatmap!(ax2,x,y,γ.write(zout),clims=(-0.25,0.25),tickfont = (4, :black))
 	Mkie.Colorbar(fig[1,3], hm1, height = Mkie.Relative(0.65))
 	
 	fig
@@ -122,7 +146,7 @@ end
 md"""
 ### 2. Cube Sphere
 
-In this cell we instead use a grid that has 6 subdomains, 100x100 points each, covering the six faces of a cube. We note that this _cube sphere_ topology involves connections between subdomain that are slightly more complicated than in the first example. And we call `demo2()` which combines steps 2 and 3. 
+In this cell we instead use a grid that has 6 subdomains, 32x32 points each, covering the six faces of a cube. We note that this _cube sphere_ topology involves connections between subdomain that are slightly more complicated than in the first example. And we call `myDemos.demo2()` which combines steps 2 and 3. 
 """
 
 # ╔═╡ dacc2261-1d75-4d68-b82c-174e4fae3631
@@ -158,30 +182,23 @@ let
 end
 
 # ╔═╡ 58f95665-9687-4b4f-af99-1239818f71a3
-md"""### More"""
+md"""### More
 
-# ╔═╡ 2cba78ac-d69b-4c47-96d0-7b784c53246b
-let
-	GridVariables=GridOfOnes("PeriodicDomain",16,20;option="full")
-	GridVariables[1].ioSize
-	x=write(GridVariables[2].XC)[:,1]
-	y=write(GridVariables[2].YC)[1,:]
-	Mkie.heatmap(x,y,write(GridVariables[2].YC))
-end
+TBD
+"""
 
 # ╔═╡ Cell order:
 # ╟─0358ae4b-f941-4254-aa10-6c37bb09c464
 # ╟─b1f3ac4b-5f75-40fa-b125-186556ece946
 # ╠═1a714fba-2a8e-11ec-182f-8f85cc17b02a
-# ╟─0dbf6e21-2dd7-497f-9b0b-f67707f2e7e0
 # ╟─a0a7637f-f115-4257-a140-06dc48dd5fba
 # ╟─54030bff-45f7-43b1-b19f-7c0cd05a02ae
-# ╠═1fc99fca-3440-4765-b11d-e10eb560aff7
+# ╟─1fc99fca-3440-4765-b11d-e10eb560aff7
 # ╟─cef83984-80dc-4196-8edd-314885fa9448
-# ╠═2573fda7-66cb-484f-9d06-a9bb85a26a9e
+# ╟─2573fda7-66cb-484f-9d06-a9bb85a26a9e
+# ╠═7c514f3b-f04d-49e3-9640-d17fdef392e6
 # ╟─2ee0a449-fd98-43e1-90b4-b4e15f237d67
 # ╟─dacc2261-1d75-4d68-b82c-174e4fae3631
 # ╟─d37ae153-8646-44ae-abd9-7e0e7d479275
-# ╠═23198b8d-6bf0-4af9-91d0-7886c9574f81
+# ╟─23198b8d-6bf0-4af9-91d0-7886c9574f81
 # ╟─58f95665-9687-4b4f-af99-1239818f71a3
-# ╠═2cba78ac-d69b-4c47-96d0-7b784c53246b
