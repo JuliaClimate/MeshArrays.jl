403ff6c7d5c8fb9786d5c0350d1f6e372327b507
gaelforget <gforget@mit.edu>
2021-11-12 22:10:24 -0500

    expand the vector field examples

--- examples/basics.jl
+++ examples/basics.jl
@@ -227,7 +227,7 @@ begin
 end
 
 # ╔═╡ 8f7ad783-f81e-4904-b848-3089216ab6e5
-begin
+let
 	DD=Interpolate(Γ_c.Depth,f,i,j,w)
 	DD=reshape(DD,size(lon))
 	DD[findall(DD.==0.0)].=NaN
@@ -281,42 +281,6 @@ let
 	println("\n The result of `exchange()` may look like this: \n\n ")
 		show(Dexch)
 	end
-end
-
-# ╔═╡ 5c755d83-6729-4de4-8038-8619b53795ff
-begin 
-	function demo_Tr(γ,Γ)
-		TrspX=γ.read(γ.path*"TrspX.bin",MeshArray(γ,Float32))
-		TrspY=γ.read(γ.path*"TrspY.bin",MeshArray(γ,Float32))
-		#TauX=γ.read(γ.path*"TauX.bin",MeshArray(γ,Float32))
-		#TauY=γ.read(γ.path*"TauY.bin",MeshArray(γ,Float32))
-		#SSH=γ.read(γ.path*"SSH.bin",MeshArray(γ,Float32))
-
-		la=-89.0:89.0
-	    LC=LatitudeCircles(la,Γ)
-	    UV=Dict("U"=>TrspX,"V"=>TrspY,"dimensions"=>["x","y"])
-
-		Tr=Array{Float64,1}(undef,length(LC));
-		[Tr[i]=ThroughFlow(UV,LC[i],Γ) for i=1:length(LC)]
-		
-		fig = Mkie.Figure(resolution = (600,400), backgroundcolor = :grey95)
-		ax = Mkie.Axis(fig[1,1], title="Meridional Ocean Transport", 
-				ylabel="Sv (1 Sv = 10^6 m^3/s)",xlabel="latitude")
-		Mkie.lines!(ax,la,Tr/1e6)
-		fig
-	end
-	
-	fig_Tr=demo_Tr(γ_c,Γ_c)
-		
-	md"""### 6. Transport Computations.
-	
-	This cell demonstrates methods that compute transports within the Earth System. Accurate transport computations involve factoring in the correct grid scaling factors, and integrating along non-trivial grid line paths [Forget et al 2015](https://doi.org/10.5194/gmd-8-3071-2015).
-	
-	!!! note
-		For a deeper dive into transport computations with `MeshArrays.jl`, see e.g. the [Julia Climate Nodetbooks](https://juliaclimate.github.io/GlobalOceanNotebooks/)
-	
-	$(fig_Tr)
-	"""
 end
 
 # ╔═╡ 4bb0c25c-336c-4438-87df-a93344ea3cfb
@@ -1505,7 +1469,6 @@ version = "3.5.0+0"
 # ╟─8f7ad783-f81e-4904-b848-3089216ab6e5
 # ╟─2c29ba59-ffc9-4763-8405-250029016ca5
 # ╠═169f9cdd-28f1-4574-ade4-237eab46a541
-# ╟─5c755d83-6729-4de4-8038-8619b53795ff
 # ╟─58f95665-9687-4b4f-af99-1239818f71a3
 # ╠═62f2dc32-456e-4e8a-8c4a-3a0ae236af13
 # ╠═4bb0c25c-336c-4438-87df-a93344ea3cfb
--- examples/vectors.jl
+++ examples/vectors.jl
@@ -16,8 +16,61 @@ begin
 	"Done with packages"
 end
 
+# ╔═╡ 7046b9b2-06bf-4ed8-80a3-79d2c52dacaf
+md"""# Vector Field Operations
+
+The flow of various quantities within the climate system is often represented as gridded vector fields. On [C-grids](https://en.wikipedia.org/wiki/Arakawa_grids), which are the most commonly used grids in global models, velocity points are staggered (shifted) by half a gridded point compared to tracer points (e.g., temperature fields). 
+
+Vector fields on the C-grid are natively oriented along the grid cell axess. That is they are locally perpendicular to the grid cell edge where they are located. Often for plotting purposes or simply to facilitate analysis of model output by users, it is convenient to rotate these vector fields along Eastward and Northward axes. Doing this requires averaging them to grid cell centers first and then applying a rotation. Function `UVtoUEVN` is provided to this end. 
+
+The result can be easily be interpolated  to any geographic position using `Interpolate`, and `InterpolationFactors` provides a mean to speed-up this step when batch processing a lot of output by pre-computing interpolation factor once and for all.
+
+Another processing step that is often useful is to convert velocities (e.g. in m/s) to transports (e.g. in m^3/s) by multiplying each value by the corresponding grid cell area. That's what function `UVtoTransport` does.
+
+The result can then be passed to functions like `ScalarPotential` and `VectorPotential` that are useful to isolate particular transport components. These higher level functions are illustrated later on in this tutorial.
+
+Transports can also be integrated along model grid line paths that may for examples connect two geographic locations, or track latitude circles, as explained in [Forget et al 2015](https://doi.org/10.5194/gmd-8-3071-2015). Functionalities to this end include `LatitudeCircles` (compute grid edge paths that track latitude circles) and `ThroughFlow` (integrate transports accross such grid edge paths).
+"""
+
 # ╔═╡ 3b6fcab2-ae44-41c7-adec-424025092419
 PlutoUI.TableOfContents()
+
+# ╔═╡ 1e7616f8-65ec-4cfa-960f-bd26451756bc
+md"""## Eastward U , Northward V"""
+
+# ╔═╡ 5d25c473-eb1f-4040-86c7-86dee7add5df
+md"""## Meridional Transports
+
+Two widely used ways of presenting global scale transports are demonstrated here :
+
+- the so-called Meridional Overturning Circulation (`MOC`) is often defined by a streamfunction [e.g. Fig 1 in Rousselet et al 2021](https://doi.org/10.1126/sciadv.abf5478). The simplest approach consists in integrating transports across lines of constant latitude (e.g., using `ThroughFlow`) and then vertically starting from the sea floor as shown below.
+- a related computation is that of net meridional transports which were recently discussed in detail for heat transport in [Forget and Ferreira 2019](https://doi.org/10.1038/s41561-019-0333-7). The case of sea water volume (~ proportional to mass) is shown below which also corresponds to the meridional streamfunction once intergrated all the way to the sea surface.
+- a third related computation, maybe more suprisingly, is the computation of transports convergence ξ. In the time mean, ξ corresponds in first approximation to the map of evaporation-precipitation-runoff plus a globally homogeneous rate of sea level change. Integrating the anomaly in ξ from the South Pole one in turn recovers the total Northward transport (see [Forget and Ferreira 2019](https://doi.org/10.1038/s41561-019-0333-7) for more on this).
+
+!!! note
+    Results are here plotted in Sverdrup units defined as 1 Sv = $10^6$ m$^3$/s
+"""
+
+# ╔═╡ 1cd94bce-9b10-4957-95d0-f396264df162
+md"""## Helmholtz Decomposition
+
+By applying the [Helmholtz Decomposition](https://en.wikipedia.org/wiki/Helmholtz_decomposition) to vertically integrated transports over the Global Ocean, we can decompose this vector field into rotational and divergent components. The divergent component gives the above map of inferred Evaporation-Precipitation-Runoff. The rotational component in turn gives the streamfunction shown below.
+"""
+
+# ╔═╡ e59c1819-52fd-49eb-8b0c-63fbbc0baffb
+md"""## x-direction U, y-direction V
+
+The maps below reflect the native velocity field orientation. Note how this differ from the Eastward U , Northward V plots shown at the top. The apparent discontinuities in the maps below simply reflect that neighboring subdomains can have different orientaton as is evident in [Forget et al 2015](https://doi.org/10.5194/gmd-8-3071-2015). 
+
+!!! note
+    Normally before interpolation one wants to apply rotation to convert to Eastward U , Northward V (see at the top of this notebook). The plot below reflects what happens if one doesn't.
+"""
+
+# ╔═╡ a8ebbe41-1ac8-44da-b8c4-cbfd4d422227
+md"""## Preliminary Steps
+
+Here we load `MeshArrays.jl` and packages that provide a climatological estimate of ocean currents on the global grid defined in [Forget et al 2015](https://doi.org/10.5194/gmd-8-3071-2015) which is then used to demonstrate vector operations listed above.
+"""
 
 # ╔═╡ 3c2de9d4-b091-464b-9210-aa84f3d4c5f1
 begin
@@ -80,37 +133,7 @@ begin
 
 	(Utr,Vtr)=MeshArrays.UVtoTransport(U,V,Γ)
 
-	"Done reading velocities, and transports"
-end
-
-# ╔═╡ 09dff33d-1c16-4fa6-a5af-8eb3c15816f3
-begin
-	nz=size(Γ.hFacC,2); nt=12; nl=length(LC)
-	ov=Array{Float64,2}(undef,nl,nz)
-	
-	#integrate across latitude circles
-	for z=1:nz
-		UV=Dict("U"=>Utr[:,z],"V"=>Vtr[:,z],"dimensions"=>["x","y"])
-		[ov[l,z]=ThroughFlow(UV,LC[l],Γ) for l=1:nl]
-	end
-	
-	#integrate from bottom
-	ov=reverse(cumsum(reverse(ov,dims=2),dims=2),dims=2)
-
-	"Done with overturning"
-end
-
-# ╔═╡ a6f70839-b79f-42e1-bf4a-8c8978e6618e
-let
-	x=vec(-89.0:89.0); y=reverse(vec(Γ.RF[1:end-1])); #coordinate variables
-	z=reverse(ov,dims=2); z[z.==0.0].=NaN
-
-	fig1 = Mkie.Figure(resolution = (900,600),markersize=0.1)
-	ax1 = Mkie.Axis(fig1[1,1], title="Overturning mean (Eulerian; in Sv)")
-	hm1=Mkie.contourf!(ax1,x,y,1e-6*z,levels=(-40.0:5.0:40.0),clims=(-40,40))
-	Mkie.Colorbar(fig1[1,2], hm1, height = Mkie.Relative(0.65))
-	fig1
-	#savefig("MOC_mean.png")
+	"Done reading velocities, and computing transports"
 end
 
 # ╔═╡ dda89e9a-4585-49df-82cd-72db00c341fa
@@ -139,15 +162,165 @@ let
 		fig1
 end
 
+# ╔═╡ 09dff33d-1c16-4fa6-a5af-8eb3c15816f3
+begin
+	nz=size(Γ.hFacC,2); nt=12; nl=length(LC)
+	ov=Array{Float64,2}(undef,nl,nz)
+	
+	#integrate across latitude circles
+	for z=1:nz
+		UV=Dict("U"=>Utr[:,z],"V"=>Vtr[:,z],"dimensions"=>["x","y"])
+		[ov[l,z]=ThroughFlow(UV,LC[l],Γ) for l=1:nl]
+	end
+	
+	#integrate from bottom
+	ov=reverse(cumsum(reverse(ov,dims=2),dims=2),dims=2)
+
+	"Done with overturning"
+end
+
+# ╔═╡ a6f70839-b79f-42e1-bf4a-8c8978e6618e
+let
+	x=vec(-89.0:89.0); y=reverse(vec(Γ.RF[1:end-1])); #coordinate variables
+	z=reverse(ov,dims=2); z[z.==0.0].=NaN
+
+	fig1 = Mkie.Figure(resolution = (900,600),markersize=0.1)
+	ax1 = Mkie.Axis(fig1[1,1], title="Meridional Overturning Streamfunction (in Sv)")
+	hm1=Mkie.contourf!(ax1,x,y,1e-6*z,levels=(-40.0:5.0:40.0),clims=(-40,40))
+	Mkie.Colorbar(fig1[1,2], hm1, height = Mkie.Relative(0.65))
+	fig1
+	#savefig("MOC_mean.png")
+end
+
+# ╔═╡ 923568dc-85f3-43c1-a631-d408a1662fb3
+begin
+	MT=fill(0.0,nl)
+	
+	#integrate across latitude circles and depth
+	for z=1:nz
+		UV=Dict("U"=>Utr[:,z],"V"=>Vtr[:,z],"dimensions"=>["x","y"])
+		[MT[l]=MT[l]+ThroughFlow(UV,LC[l],Γ) for l=1:nl]
+	end
+	
+	"Done with meridional transport"
+end
+
+# ╔═╡ 2d6c1787-3c30-4daf-8575-b51211f8e860
+let
+	x=vec(-89.0:89.0)
+	fig1 = Mkie.Figure(resolution = (900,600),markersize=0.1)
+	ax1 = Mkie.Axis(fig1[1,1], title="Northward Volume Transport (in Sv)")
+	hm1=Mkie.lines!(x,1e-6*MT,xlabel="latitude",ylabel="Transport (in Sv)",label="ECCO estimate")
+	fig1
+end
+
+# ╔═╡ 3a3ac112-cd2b-4ac9-9029-b79b8b5fa8b4
+begin
+	Tx=0.0*Utr[:,1]
+	Ty=0.0*Vtr[:,1]
+	for z=1:nz
+		Tx=Tx+Utr[:,z]
+		Ty=Ty+Vtr[:,z]
+	end
+
+	#convergence & land mask
+	TrspCon=μ.*convergence(Tx,Ty)
+	
+	#scalar potential
+	TrspPot=ScalarPotential(TrspCon)
+	
+	#Divergent transport component
+	(TxD,TyD)=gradient(TrspPot,Γ)
+	TxD=TxD.*Γ.DXC
+	TyD=TyD.*Γ.DYC
+	
+	#Rotational transport component
+	TxR = Tx-TxD
+	TyR = Ty-TyD
+	
+	#vector Potential
+	TrspPsi=μ.*VectorPotential(TxR,TyR,Γ)
+
+	"Done with convergence and potentials"
+end
+
+# ╔═╡ 9928d59f-0733-4ad9-93e7-f1a8622afd1f
+let
+		z=reshape(Interpolate(TrspCon,λ.f,λ.i,λ.j,λ.w),size(λ.lon))
+
+		fig1 = Mkie.Figure(resolution = (900,600),markersize=0.1)
+		ax1 = Mkie.Axis(fig1[1,1], title="Evaporation - Precipitation - Runoff (in Sv; inferred)")
+		hm1=Mkie.contourf!(ax1,λ.lon[:,1],λ.lat[1,:],z,levels=(-1.0:0.25:1.0).*800.0,clims=(-1.0,1.0).*800.0)
+		Mkie.Colorbar(fig1[1,2], hm1, height = Mkie.Relative(0.65))
+		fig1
+end
+
+# ╔═╡ c3b3a104-a1d6-4692-ae0c-2e55821afd03
+let
+	z=reshape(Interpolate(1e-6*TrspPsi,λ.f,λ.i,λ.j,λ.w),size(λ.lon))
+
+	fig1 = Mkie.Figure(resolution = (900,600),markersize=0.1)
+	ax1 = Mkie.Axis(fig1[1,1], title="Streamfunction (in Sv)")
+	hm1=Mkie.contourf!(ax1,λ.lon[:,1],λ.lat[1,:],z,levels=(-1.0:0.25:1.0).*40.0,clims=(-1.0,1.0).*40.0)
+	Mkie.Colorbar(fig1[1,2], hm1, height = Mkie.Relative(0.65))
+	fig1
+end
+
+# ╔═╡ e033825c-019c-44b6-86dd-30fff79f8339
+let
+	#(dDdx, dDdy)=gradient(1e6*TrspCon,Γ)
+	dDdx=U[:,1]
+	dDdy=V[:,1]
+
+	DD=Interpolate(dDdx,λ.f,λ.i,λ.j,λ.w)
+	DD=reshape(DD,size(λ.lon))
+	DD[findall(DD.==0.0)].=NaN
+	dDdx_i=DD
+
+	DD=Interpolate(dDdy,λ.f,λ.i,λ.j,λ.w)
+	DD=reshape(DD,size(λ.lon))
+	DD[findall(DD.==0.0)].=NaN
+	dDdy_i=DD
+
+	fig = Mkie.Figure(resolution = (900,900), backgroundcolor = :grey95)
+	ax = Mkie.Axis(fig[1,1], title="x-direction velocity (in m/s)",xlabel="longitude",ylabel="latitude")
+	hm1=Mkie.heatmap!(ax,λ.lon[:,1],λ.lat[1,:],dDdx_i,colorrange=(-1.0,1.0).*0.2)
+	ax = Mkie.Axis(fig[2,1], title="y-direction velocity (in m/s)",xlabel="longitude",ylabel="latitude")
+	hm1=Mkie.heatmap!(ax,λ.lon[:,1],λ.lat[1,:],dDdy_i,colorrange=(-1.0,1.0).*0.2)
+	Mkie.Colorbar(fig[1:2,2], hm1, height = Mkie.Relative(0.65))
+	fig
+end
+
+# ╔═╡ 87a4a232-d8eb-4612-8cc5-dc9d1c16dbe8
+md"""## Particle Tracking
+
+Materials and particles that tend to follow ocean currents or atmospheric winds can be analyzed in terms of trajectories. These are simply computed by integrating velocities over time within a [Lagrangian framework](https://en.wikipedia.org/wiki/Lagrangian_and_Eulerian_specification_of_the_flow_field).
+
+When vector fields are represented using [MeshArrays.jl](https://github.com/JuliaClimate/MeshArrays.jl), as done in this tutorial, this is easily done via the [IndividualDisplacements.jl](https://github.com/JuliaClimate/IndividualDisplacements.jl) package as shown in [these examples](https://juliaclimate.github.io/IndividualDisplacements.jl/dev/examples/).
+"""
+
 # ╔═╡ Cell order:
-# ╟─71b1447b-39d9-46e2-966d-a1e6e8dcccc6
+# ╟─7046b9b2-06bf-4ed8-80a3-79d2c52dacaf
 # ╟─3b6fcab2-ae44-41c7-adec-424025092419
+# ╟─1e7616f8-65ec-4cfa-960f-bd26451756bc
+# ╟─dda89e9a-4585-49df-82cd-72db00c341fa
+# ╟─60bda89f-5452-4f89-b4a4-a89568d45a6b
+# ╟─99e7e24b-578e-47be-818a-623c8e9e4381
+# ╟─5d25c473-eb1f-4040-86c7-86dee7add5df
+# ╟─09dff33d-1c16-4fa6-a5af-8eb3c15816f3
+# ╟─a6f70839-b79f-42e1-bf4a-8c8978e6618e
+# ╟─923568dc-85f3-43c1-a631-d408a1662fb3
+# ╟─2d6c1787-3c30-4daf-8575-b51211f8e860
+# ╟─3a3ac112-cd2b-4ac9-9029-b79b8b5fa8b4
+# ╟─9928d59f-0733-4ad9-93e7-f1a8622afd1f
+# ╟─1cd94bce-9b10-4957-95d0-f396264df162
+# ╟─c3b3a104-a1d6-4692-ae0c-2e55821afd03
+# ╟─e59c1819-52fd-49eb-8b0c-63fbbc0baffb
+# ╟─e033825c-019c-44b6-86dd-30fff79f8339
+# ╟─87a4a232-d8eb-4612-8cc5-dc9d1c16dbe8
+# ╟─a8ebbe41-1ac8-44da-b8c4-cbfd4d422227
+# ╟─71b1447b-39d9-46e2-966d-a1e6e8dcccc6
 # ╟─3c2de9d4-b091-464b-9210-aa84f3d4c5f1
 # ╟─c2788db6-3ab4-4c22-abf0-ae701a57e94d
 # ╟─cf99ec64-d142-42ff-9767-3d851229024e
 # ╟─ea72e6df-68da-4ade-b1d9-b7689e511a40
-# ╟─09dff33d-1c16-4fa6-a5af-8eb3c15816f3
-# ╟─a6f70839-b79f-42e1-bf4a-8c8978e6618e
-# ╟─dda89e9a-4585-49df-82cd-72db00c341fa
-# ╟─60bda89f-5452-4f89-b4a4-a89568d45a6b
-# ╟─99e7e24b-578e-47be-818a-623c8e9e4381
