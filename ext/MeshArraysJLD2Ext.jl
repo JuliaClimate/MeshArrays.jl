913d36b42cfe12d18c49f618c68230c26d30ae68
gaelforget <gforget@mit.edu>
2023-08-30 12:09:25 -0400

    add documentation

--- ext/MeshArraysJLD2Ext.jl
+++ ext/MeshArraysJLD2Ext.jl
@@ -4,7 +4,18 @@ module MeshArraysJLD2Ext
     using MeshArrays, JLD2
     import MeshArrays: read_JLD2, write_JLD2
 
+    """
+       write_JLD2(file;kwargs...)
+
+    Call `JLD2.jldsave(file;kwargs...)`
+    """
     write_JLD2(fil;kwargs...) = jldsave(fil;kwargs...)
+
+    """
+       read_JLD2(file)
+
+    Call `FileIO.load(file)`. Works `.jld2`, `.jpg`, etc files.
+    """
     read_JLD2(a) = load(a)
 
 end
