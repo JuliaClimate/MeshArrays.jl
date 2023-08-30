793d1acac5fa40cc4789a85b103973e855586cc9
gaelforget <gforget@mit.edu>
2023-08-30 00:07:55 -0400

    fix write_JLD2 synthax

--- ext/MeshArraysJLD2Ext.jl
+++ ext/MeshArraysJLD2Ext.jl
@@ -4,7 +4,7 @@ module MeshArraysJLD2Ext
     using MeshArrays, JLD2
     import MeshArrays: read_JLD2, write_JLD2
 
-    write_JLD2(a...) = jldsave(a...)
+    write_JLD2(fil;kwargs...) = jldsave(fil;kwargs...)
     read_JLD2(a) = load(a)
 
 end
