6621efa4534edd27d93bb2289e2a928fe521b3ae
Jonathan Lauderdale <jonathan.lauderdale@gmail.com>
2025-05-09 08:47:33 -0400

    add docstring for `filename`

--- src/Interpolation.jl
+++ src/Interpolation.jl
@@ -241,6 +241,9 @@ Download or recompute interpolation coefficients.
 
 - `λ=interpolation_setup()` to download "interp_coeffs_halfdeg.jld2" 
 - `λ=interpolation_setup(Γ=Γ)` to recompute interpolation to `lon,lat`
+- `λ=interpolation_setup(Γ=Γ,lon=lon,lat=lat,filename=filename)` to 
+    recompute interpolation to `lon,lat` and save to location and file `filename`,
+    defaulting to `tempname()*"_interp_coeffs.jld2"`.
 """
 function interpolation_setup(;Γ=NamedTuple(),
         lon=[i for i=-179.:2.0:179., j=-89.:2.0:89.],
