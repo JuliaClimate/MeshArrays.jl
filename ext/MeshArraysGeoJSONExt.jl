e73a362ed5b03dae0b0cd1a0ef40f21d8f8bd4d8
gaelforget <gforget@mit.edu>
2025-12-01 06:55:40 -0500

    example use case for point in polygon

--- ext/MeshArraysGeoJSONExt.jl
+++ ext/MeshArraysGeoJSONExt.jl
@@ -17,8 +17,20 @@ module MeshArraysGeoJSONExt
     - if `native` then skip conversion, just call `GeoJSON.read`.
 
     ```
-    pol=MeshArrays.Dataset("countries_geojson1",do_read=false)
+    import MeshArrays, DataDeps, GeoJSON
+    pol=MeshArrays.Dataset("oceans_geojson1",do_read=false)
     pol=MeshArrays.read_json(pol,native=true)
+
+    ii=11
+    nam=pol[ii].name
+    geom=pol[ii].geometry
+
+    import GeometryOps as GO, GeoInterface as GI
+    rule_pol = (x,y) -> GO.within(GI.Point(x,y), geom)
+    rule_pol_vec = (x,y) -> rule_pol.(x,y)
+
+    np=1000; lo=-180 .+360*rand(np); la=-90 .+180*rand(np);
+    np_in=sum(rule_pol.(lo,la)); [np_in np-np_in nam]
     ```
     """
     function read_json(fil; native=false)
