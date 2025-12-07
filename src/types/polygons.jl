3bae543b0434f9fe13a2d3001d803c3355d8d5e3
gaelforget <gforget@mit.edu>
2025-12-06 20:51:35 -0500

    to_Polygon, to_polyarray, docs

--- src/types/polygons.jl
+++ src/types/polygons.jl
@@ -5,21 +5,6 @@ struct NamedPolygon
     name::String
     points::Vector{}
 end
-
-Use example:
-
-```
-pol=MeshArrays.Dataset("oceans_geojson1")
-g=pol[1].geometry
-p=MeshArrays.GI.coordinates(g)[1]
-n=pol[1].name
-npol1=MeshArrays.NamedPolygon(p,n,p)
-apol1=MeshArrays.polyarray("anonymous",[npol1 npol1])
-
-using CairoMakie
-MeshArraysMakieExt = Base.get_extension(MeshArrays, :MeshArraysMakieExt)
-lines(MeshArraysMakieExt.pol_to_Makie(apol1))
-```
 """
 struct NamedPolygon
     geometry::Vector{Union{Tuple,Vector}}
@@ -28,24 +13,44 @@ struct NamedPolygon
 end
 
 """
-    polyarray
-
-polyarray data structure.
-
-```
 struct polyarray
    name::String
    f::Array{NamedPolygon}
 end
-```
 
+```
+using MeshArrays, GeoJSON, DataDeps
+pol=MeshArrays.Dataset("oceans_geojson1")
+apol=MeshArrays.to_polyarray(pol)
+
+using CairoMakie
+MeshArraysMakieExt = Base.get_extension(MeshArrays, :MeshArraysMakieExt)
+lines(MeshArraysMakieExt.pol_to_Makie(apol))
+```
 """
 struct polyarray
    name::String
    f::Array{NamedPolygon}
 end
 
+##
+
+function to_polyarray(pol)
+    nk=length(pol)
+    npol=Array{NamedPolygon}(undef, nk)
+    for k in 1:nk
+        g=pol[k].geometry
+        p=MeshArrays.GI.coordinates(g)[1]
+        n=pol[k].name
+        npol[k]=MeshArrays.NamedPolygon(p,n,p)
+    end
+    MeshArrays.polyarray("anonymous",npol)
+end
+
+##
+
 """
+    to_Polygon(apol::polyarray)
 
 ```
 using DataFrames
@@ -59,11 +64,11 @@ function write_polygons_to_json(pols,nams)
         GeoJSON.write(fn, df)
 end
 
-pols,nams=MeshArrays.to_pol(apol1)
+pols,nams=MeshArrays.to_Polygon(polyarray)
 write_polygons_to_json(pols,nams)
 ```
 """
-function to_pol(apol1)
+function to_Polygon(apol1)
     nk=length(apol1.f)
 	pols=Array{GI.Wrappers.Polygon}(undef, nk)
     nams=Array{String}(undef, nk)
