e730b4b7ed66f59f749fdca188809c96cc8a9477
gaelforget <gforget@mit.edu>
2026-06-05 08:18:54 -0400

    fix uv exchange call

--- src/exchanges/main.jl
+++ src/exchanges/main.jl
@@ -31,16 +31,14 @@ function exchange(u::AbstractMeshArray,v::AbstractMeshArray)
 	uu=MeshArray_wh(similar(u),1)
   vv=MeshArray_wh(similar(v),1)
 	if length(size(v))==1
-    tmp=exchange_main(u).MA
-		[uu.MA.f[k]=tmp.f[k] for k in 1:length(u)] 
-    tmp=exchange_main(v).MA
-		[vv.MA.f[k]=tmp.f[k] for k in 1:length(v)] 
+    tmp_u,tmp_v=exchange_main(u,v)
+		[uu.MA.f[k]=tmp_u.f[k] for k in 1:length(u)] 
+		[vv.MA.f[k]=tmp_v.f[k] for k in 1:length(v)] 
 	else
 		for k in 1:size(v)[2]
-      tmp=exchange_main(u[:,k]).MA
-			[uu.MA.f[kk,k]=tmp.f[kk] for kk in 1:size(u,1)] 
-      tmp=exchange_main(v[:,k]).MA
-			[vv.MA.f[kk,k]=tmp.f[kk] for kk in 1:size(v,1)] 
+      tmp_u,tmp_v=exchange_main(u[:,k],vu[:,k])
+			[uu.MA.f[kk,k]=tmp_u.MA.f[kk] for kk in 1:size(u,1)] 
+			[vv.MA.f[kk,k]=tmp_v.MA.f[kk] for kk in 1:size(v,1)] 
 		end
 	end
 	uu,vv
