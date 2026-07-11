0e288e4cf72a96a7f786e3ff9cf8da800ef42fba
gaelforget <gforget@mit.edu>
2026-07-11 18:28:11 +0200

    add missing file

---
+++ test/setup.jl
@@ -0,0 +1,15 @@
+using Test, Documenter, Suppressor, MeshArrays, CairoMakie
+import DataDeps, JLD2, Shapefile, GeoJSON, Proj, GeometryOps
+import MITgcm
+import MeshArrays: GI, NEMO_GRID, Grids_simple
+
+MeshArrays.Dataset("GRID_LL360")
+MeshArrays.Dataset("GRID_LLC90")
+MeshArrays.Dataset("GRID_LLC270")
+MeshArrays.Dataset("GRID_CS32")
+
+pol_shp=MeshArrays.Dataset("countries_shp1")
+pol_json=MeshArrays.Dataset("oceans_geojson1")
+
+p=dirname(pathof(MeshArrays))
+include(joinpath(p,"../examples/Demos.jl"))
